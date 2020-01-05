503d99c5be452c15d6ef3aacb18ac7927ba0d0ae
avanlinden <11965371+avanlinden@users.noreply.github.com>
2020-01-05 17:19:20 -0500

    fixed a few code errors; everything should run

--- brachymeles_PGLS.R
+++ brachymeles_PGLS.R
@@ -26,12 +26,15 @@ plot(silerPhy$BI, cex = 0.6)
 # We'll work with the maximum likelihood tree for our analysis
 
 
-
 ### Load body shape data =============================
 
 skinkData <- read.csv(file = "skink_measurements.csv")
 
+#format as tibble
 skinkDat <- as_tibble(skinkData)
+
+#check
+skinkDat
 
 #reformat species names to match tree tip labels with underscore
 
@@ -40,7 +43,7 @@ skinkDat <- skinkDat %>%
   mutate(species = as.character(species)) %>% 
   mutate(species = str_replace(species, " ", "_"))
 
-# write cleaned data to file
+# write cleaned data to file if you want
 write.csv(skinkDat, file = "cleaned_Brachymeles_data.csv")
 
 #create a tibble with the species names as they appear in the measurement data (we will check this against tree names later)
@@ -53,7 +56,8 @@ datSpec <- skinkDat %>%
 #make a vector of the tree tips
 evoTips <- silerPhy$ML$tip.label
 
-#make that vector into a tibble for easy string manipulation, I'm not here to do stuff with strings in base R
+# make that vector into a tibble for easy string manipulation, I'm not here to do stuff with strings in base R 
+# (it will get mad that you made a vector into a tibble but it will still do it; if it ever stops working, replace "as_tibble" with "enframe")
 treeSpec <- evoTips %>% 
   as_tibble() %>% 
   dplyr::rename(species = value) 
@@ -73,16 +77,15 @@ skinkTree <- silerPhy$ML
 skinkTree$tip.label <- treeSpecEdit$species
 
 #now the tip labels are in our format
-plot(ladderize(skinkTree), cex = 0.5)
+plot(skinkTree, cex = 0.5)
 
 #CHECK -- are all of our sample skinks in the tree?
-
 treeSpecEdit %>% 
   filter(species %in% c(datSpec$species)) %>% 
   dplyr::select(species) %>% 
-  unique() #ok so for whatever reason only 8 are in the tree...I assume it's spelling
+  unique() #ok so for whatever reason only 8 are in the tree (spelling, synonym issues)
 
-#there is no B. vermis because it's not in the tree -- no tissue samples exist and restricted access to island
+#there is no B. vermis because it's not in the tree -- no tissue samples exist and restricted access to island; we have to drop B. vermis from our sample data because we have no phylo info for it
 
 ## start editing some tree tip names to correspond to our specimen names (resolve synonyms, misspellings, etc)
 
@@ -153,7 +156,6 @@ names(subtrees)<-nodes ## all subtrees
 
 all.foos <- c()
 
-
 all.foos<-nodes[sapply(subtrees,function(x) all(x$tip.label %in% taxa))] #this is actully just all the nodes
 
 foo.mrcas<-monoNodes[sapply(monoNodes,function(x,tree,y)
@@ -166,14 +168,16 @@ w.foos<-which(tree$tip.label %in% taxa)
 plot(tree, cex = 0.6)
 nodelabels(cex = 0.5)
 
-#nodes of species monophyly:
+#nodes of species monophyly (by visual inspection aka "looking" with my "eyes"):
 
 monoNodes <- c(42, 43, 46, 47, 51, 54, 57, 61, 66, 68, 71, 74)
 
 ## rename tips uniquely
-tree$tip.label[w.foos]<-paste(tree$tip.label[w.foos],
-                              replicate(length(w.foos),paste(sample(letters,6),
-                                                             collapse="")),sep="_")
+tree$tip.label[w.foos] <- paste(tree$tip.label[w.foos],
+                                replicate(length(w.foos), paste(sample(letters, 6),
+                                                                collapse =
+                                                                  "")), sep = "_")
+collapsed <- tree
 
 ## iterate over all MRCAs of foo clades
 for(i in 1:length(foo.mrcas)){
@@ -215,15 +219,17 @@ write.tree(theTree, file = "pruned_renamed_Brachymeles.nex")
 #If needed, load the CORRECT tree file:
 theTree <- read.tree(file = "pruned_renamed_Brachymeles.nex")
 
-# Load the CLEANED and CORRECT data:
-skinkDat <- as.tibble(read.csv("cleaned_Brachymeles_data.csv"))
+# Load the CLEANED and CORRECT data if needed:
+skinkDat <- as_tibble(read.csv("cleaned_Brachymeles_data.csv"))
 
+#tidy data and reformat species names if you reloaded
 skinkData <- skinkDat %>% 
   dplyr::select(-X) %>% 
   mutate(species = str_replace(species, " ", "_")) %>% 
   as.data.frame()
 
 #create the comparative data object
+#there will be a message about data being dropped -- this is because B. vermis is not in our tree
 skinkComp <- comparative.data(phy = theTree, data = skinkData, names.col = species, vcv = TRUE, warn.dropped = TRUE)
 
 ### PGLS models for area vs body size ==============
@@ -279,10 +285,6 @@ write.csv(pglsAreaResults, file = "skink_area_pgls_results.csv")
 
 ### PGLS models for CIRCUMFERENCE vs SVL ==================
 
-skinkDat %>% 
-  ggplot(aes(log10(SVLDig), log10(headCirc))) +
-  geom_point()
-
 headC <- pgls(log10(headCirc) ~ log10(SVLDig), data = skinkComp, lambda = "ML")
 summary(headC)
 plot(pgls.profile(headC, which = "lambda")) #this is a plot of the likelihood surface of the estimates for lambda, it's not important for the outcome of the model
@@ -290,11 +292,10 @@ plot(pgls.profile(headC, which = "lambda")) #this is a plot of the likelihood su
 midC <- pgls(log10(midDorsalCirc) ~ log10(SVLDig), data = skinkComp, lambda = "ML")
 summary(midC)
 
-tail <- pgls(log10(midtailirc) ~ log10(SVLDig), data = skinkComp, lambda = "ML")
-summary(tail) 
+tailC <- pgls(log10(midTailCirc) ~ log10(SVLDig), data = skinkComp, lambda = "ML")
+summary(tailC) 
 
 #gather and store pgls results: intercept estimate and p-value, slope (SVL) estimate and p-value, lambda estimate, lambda 95% CI intervals, adjusted R-squared)
-
 
 row_vars = c("Int_est","Int_p", "slope_est", "slope_p", "lambda_est", "lambda_ci_low", "lambda_ci_high", "adj_rsq")
 
